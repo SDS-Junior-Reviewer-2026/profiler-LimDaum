1f3cb7bb5452972a6a0aaceea32972c569eed873
ddaumm <dlaekdna4862@gmail.com>
2026-03-30 17:41:22 +0900

    수정/개선 : SortServiceLibrary.java 변수 이름 수정 및 알고리즘 개선

--- src/main/java/org/example/SortServiceLibrary.java
+++ src/main/java/org/example/SortServiceLibrary.java
@@ -22,19 +22,18 @@ interface SortServiceLib {
 class SelectionSort implements SortServiceLib {
     private int changeCnt = 0;
 
-    // selection sort 오름차순 예시코드
     public void sort(ArrayList<Integer> arr) {
-        int tmp_min_idx;
-        for (int i = 0; i < arr.size(); i++) {
-            tmp_min_idx = i;
+        int minIdx;
+        for (int i = 0; i < arr.size()-1; i++) {
+            minIdx = i;
             for (int j = i + 1; j < arr.size(); j++) {
-                if (arr.get(tmp_min_idx) > arr.get(j)) {
-                    tmp_min_idx = j;
+                if (arr.get(minIdx) > arr.get(j)) {
+                    minIdx = j;
                 }
             }
-            if (tmp_min_idx != i) {
+            if (minIdx != i) {
                 changeCnt++;
-                Collections.swap(arr, i, tmp_min_idx);
+                Collections.swap(arr, i, minIdx);
             }
         }
     }
@@ -42,20 +41,22 @@ class SelectionSort implements SortServiceLib {
     public int getChangeCnt() {
         return changeCnt;
     }
-
 }
 
 class BubbleSort implements SortServiceLib {
     private int changeCnt = 0;
 
-    // selection sort 오름차순 예시코드
     public void sort(ArrayList<Integer> arr) {
         for (int i = 0; i < arr.size(); i++) {
+            int tmpChangeCnt = changeCnt;
             for (int j = 0; j < arr.size()-i-1; j++) {
                 if (arr.get(j) > arr.get(j+1)) {
                     changeCnt++;
                     Collections.swap(arr, j, j+1);
                 }
+            }
+            if  (changeCnt == tmpChangeCnt) {
+                break;
             }
         }
     }
